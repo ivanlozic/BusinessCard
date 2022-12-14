2de03664e976fa5668e2ec5262a98ce76494c6c3
Ivan Lozic <ivanlozic995@gmail.com>
2022-12-14 21:49:21 +0100

    Third commit

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ function About() {
     <div>
       <h1>About</h1>
       <div>
-      <p>I am frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with best practices, and I am always looking for new things to learn</p>
+      <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with best practices, and I am always looking for new things to learn.</p>
       </div>
     </div>
   );
